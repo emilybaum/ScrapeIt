4b9aac3892aacea8539fb388041bd2a455bb9354
emilybaum <emily.r.baum@gmail.com>
2019-08-23 22:32:17 -0400

    added notes.handlebars and building in the content - also cleaned up Models

--- models/Article.js
+++ models/Article.js
@@ -2,18 +2,13 @@
 
 const mongoose = require("mongoose");
 
-// Save a reference to the Schema constructor
 let Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
 let ArticleSchema = new Schema({
-    // `title` is required and of type String
     title: {
         type: String,
         required: true
     },
-    // `link` is required and of type String
     img: {
         type: String,
         required: true
@@ -35,8 +30,6 @@ let ArticleSchema = new Schema({
     }
 });
 
-// This creates our model from the above schema, using mongoose's model method
 let Article = mongoose.model("Article", ArticleSchema);
 
-// Export the Article model
 module.exports = Article;
